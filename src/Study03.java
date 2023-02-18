0dfc7e06887e261ae59907b9cf5ccc8d50a5a45a
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-02-18 16:23:40 +0900

    create : 시저_암호

--- src/Study03.java
+++ src/Study03.java
@@ -168,7 +168,21 @@ public class Study03 {
         return answer;
     }
 
+    public String 시저_암호(String s, int n) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLowerCase(c)) {
+                c = (char) ((c - 'a' + n) % 26 + 'a');
+            } else if (Character.isUpperCase(c)) {
+                c = (char) ((c - 'A' + n) % 26 + 'A');
+            }
+            answer += c;
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
-        예산(new int[]{2,2,3,3},10);
+        예산(new int[]{2, 2, 3, 3}, 10);
     }
 }
