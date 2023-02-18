a5b7a04425a9d19c0e6e4ae755950e37ebde3c22
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-02-18 09:34:33 +0900

    create : 행렬의_덧셈

--- src/Study03.java
+++ src/Study03.java
@@ -9,30 +9,40 @@ public class Study03 {
                     break;
                 }
             }
-        }else{
+        } else {
             answer = false;
         }
         return answer;
     }
+
     public static long 부족한_금액_계산하기(int price, int money, int count) {
         long answer = -1;
         long priceResult = 0;
         long result = 0;
 
         for (int i = 1; i <= count; i++) {
-            priceResult += price*i;
-
+            priceResult += price * i;
         }
         if (money >= priceResult) {
             result = 0;
-        }else{
+        } else {
             result = Math.abs(money - priceResult);
         }
         answer = result;
         return answer;
     }
 
+    public int[][] 행렬의_덧셈(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
-        부족한_금액_계산하기(3,20,4);
+        부족한_금액_계산하기(3, 20, 4);
     }
 }
