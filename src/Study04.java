cd01beb5a7664c2ccbeb3bf46c15393c634d80be
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-02-21 13:45:38 +0900

    create : 문자열_내_마음대로_정렬하기

--- src/Study04.java
+++ src/Study04.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class Study04 {
+    //todo : 비트연산 , 자리수 포함
     public static String[] 비밀지도(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for (int i = 0; i < n; i++) {
@@ -7,6 +10,20 @@ public class Study04 {
             sarp = sarp.replaceAll("0", " ");
             answer[i] = sarp;
         }
+        return answer;
+    }
+
+    //
+    public String[] 문자열_내_마음대로_정렬하기(String[] strings, int n) {
+        String[] answer = {};
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = strings[i].charAt(n) + strings[i];
+        }
+        Arrays.sort(strings);
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = strings[i].substring(1);
+        }
+        answer = strings;
         return answer;
     }
 
