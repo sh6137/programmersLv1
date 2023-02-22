ae06303d89ac3826900ee3d807532ae782bd8cec
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-02-22 21:01:14 +0900

    create : 콜라_문제

--- src/Study05.java
+++ src/Study05.java
@@ -1,2 +1,25 @@
 public class Study05 {
+    public static int 콜라_문제(int a, int b, int n) {
+        int answer = 0;
+        while (true) {
+            int remain = 0;
+            if (n % a != 0) {
+                remain = n % a;
+            }
+            if (remain != 0) {
+                n = n - remain;
+            }
+            answer += (n / a) * b;
+            n = (n / a) * b;;
+            n = n + remain;
+            if (n < a) {
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(콜라_문제(3, 1, 20));
+    }
 }
