a1f8b5cebeb1a3e1c11c7051af7d0112b9318f23
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-02-22 21:21:43 +0900

    create : 푸드_파이트_대회

--- src/Study05.java
+++ src/Study05.java
@@ -10,12 +10,34 @@ public class Study05 {
                 n = n - remain;
             }
             answer += (n / a) * b;
-            n = (n / a) * b;;
+            n = (n / a) * b;
             n = n + remain;
             if (n < a) {
                 break;
             }
         }
+        return answer;
+    }
+
+    public String 푸드_파이트_대회(int[] food) {
+        String answer = "";
+        for (int i = 1; i < food.length; i++) {
+            if (food[i] == 1) {
+                food[i] = 0;
+            } else {
+                if (food[i] % 2 != 0) {
+                    food[i] = food[i] - 1;
+                }
+            }
+        }
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            for (int j = 0; j < count; j++) {
+                answer += String.valueOf(i);
+            }
+        }
+        StringBuilder str = new StringBuilder(answer);
+        answer = answer + "0" + str.reverse();
         return answer;
     }
 
