4dcd817d557e6e9bc7fdcbd546a3fc13effcc171
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-03-02 20:30:08 +0900

    create : 가장_가까운_같은_글자

--- src/Study06.java
+++ src/Study06.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Study06 {
 
-    public int 폰켓문(int[] nums) {
+    public int 폰켓몬(int[] nums) {
         int answer = 0;
         int count = nums.length / 2;
         HashSet<Integer> integers = new HashSet<>();
@@ -12,13 +14,41 @@ public class Study06 {
 
         if (integers.size() > count) {
             answer = count;
-        }else {
+        } else {
             answer = integers.size();
         }
         return answer;
     }
 
+
+    public static int[] 가장_가까운_같은_글자(String s) {
+        int[] answer = {};
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        String[] str = s.split("");
+        for (int i = 0; i < str.length; i++) {
+            if (i == 0) {
+                arrayList.add(-1);
+            } else {
+                int count = -1;
+                for (int j = i - 1; j >= 0; j--) {
+                    if (str[i].equals(str[j])) {
+                        arrayList.add(i - j);
+                        break;
+                    } else {
+                        if (j == 0) {
+                            arrayList.add(count);
+                        }
+                    }
+                }
+            }
+        }
+        answer = arrayList.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(가장_가까운_같은_글자("foobar")));
 
     }
 }
