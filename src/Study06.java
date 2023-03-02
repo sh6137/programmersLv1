e10565704b791c9535fc944724e990d6287f4e95
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-03-02 19:53:42 +0900

    create : 폰켓몬

--- src/Study06.java
+++ src/Study06.java
@@ -1,7 +1,20 @@
+import java.util.HashSet;
+
 public class Study06 {
 
     public int 폰켓문(int[] nums) {
         int answer = 0;
+        int count = nums.length / 2;
+        HashSet<Integer> integers = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            integers.add(nums[i]);
+        }
+
+        if (integers.size() > count) {
+            answer = count;
+        }else {
+            answer = integers.size();
+        }
         return answer;
     }
 
