5371c3131b46fd51ff56b688d39982757c15797a
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-03-07 16:12:05 +0900

    create : 소수찾기

--- src/Study06.java
+++ src/Study06.java
@@ -47,8 +47,28 @@ public class Study06 {
         return answer;
     }
 
+    public static int 소수_찾기(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            int count = 0;
+            for (int j = 2; j <= (int) Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }else{
+
+                }
+            }
+            if (count == 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(가장_가까운_같은_글자("foobar")));
+        System.out.println(소수_찾기(10));
 
     }
 }
