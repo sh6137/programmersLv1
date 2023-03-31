ac306484fd6326c17cf752125d030bb3280b978d
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-03-31 19:48:26 +0900

    create : 기사단원의_무기

--- src/Study11.java
+++ src/Study11.java
@@ -1,5 +1,29 @@
-public class Study11 {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Study11 {
+    public static int 기사단원의_무기(int number, int limit, int power) {
+        int answer = 0;
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 1; i <= number; i++) {
+            int count = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) {
+                    count++;
+                } else if (i % j == 0) {
+                    count += 2;
+                }
+            }
+            if (count > limit) {
+                count = power;
+            }
+            integerList.add(count);
+        }
+        answer = integerList.stream().mapToInt(i -> i).sum();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        기사단원의_무기(5, 3, 2);
     }
 }
