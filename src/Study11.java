3d4026422b9bb9d0f5e345af76269cd8b321697b
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-02 21:26:16 +0900

    create : 카드뭉치

--- src/Study11.java
+++ src/Study11.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Study11 {
@@ -23,7 +24,35 @@ public class Study11 {
         return answer;
     }
 
+    public static String 카드_뭉치(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        List<String> cardList1 = new ArrayList<>(Arrays.asList(cards1));
+        List<String> cardList2 = new ArrayList<>(Arrays.asList(cards2));
+        List<String> goalList = new ArrayList<>(Arrays.asList(goal));
+        for (int i = 0; i < goalList.size(); i++) {
+            if (goalList.get(i).equals(cardList1.get(0))) {
+                cardList1.remove(0);
+            } else if (goalList.get(i).equals(cardList2.get(0))) {
+                cardList2.remove(0);
+            } else {
+                answer = "No";
+            }
+
+            if (cardList1.size() == 0) {
+                cardList1.add("1");
+            }
+            if (cardList2.size() == 0) {
+                cardList2.add("2");
+            }
+        }
+        if (answer.equals("")) {
+            answer = "Yes";
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
-        기사단원의_무기(5, 3, 2);
+        System.out.println(카드_뭉치(new String[]{"i", "water", "drink"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"})
+        );
     }
 }
