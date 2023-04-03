278551ef1bbf6000b91424b2c6d739367b1892e9
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-03 21:26:28 +0900

    create : 체육복

--- src/Study12.java
+++ src/Study12.java
@@ -1,4 +1,5 @@
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Study12 {
     public static int[] 추억_점수(String[] name, int[] yearning, String[][] photo) {
@@ -18,7 +19,7 @@ public class Study12 {
         return answer;
     }
 
-    public int 덧칠하기(int n, int m, int[] section) {
+    public int 덧칠하(int n, int m, int[] section) {
         int answer = 0;
         int max = 0;
         for (int i = 0; i < section.length; i++) {
@@ -30,7 +31,35 @@ public class Study12 {
         return answer;
     }
 
+    public static int 체육복(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int count = 0;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        Set<Integer> lostSet = new HashSet<>(Arrays.stream(lost).boxed().collect(Collectors.toSet()));
+        Set<Integer> reserveSet = new HashSet<>(Arrays.stream(reserve).boxed().collect(Collectors.toSet()));
+        Set<Integer> reserveSet2 = new HashSet<>(Arrays.stream(reserve).boxed().collect(Collectors.toSet()));
+
+        reserveSet2.retainAll(lostSet);
+
+        lostSet.removeAll(reserveSet2);
+        reserveSet.removeAll(reserveSet2);
+
+        ArrayList<Integer> lostList = new ArrayList<>(lostSet);
+        ArrayList<Integer> reserveList = new ArrayList<>(reserveSet);
+
+        for (int i = 0; i < reserveList.size(); i++) {
+            if (lostList.contains(reserveList.get(i) - 1)) {
+                lostList.remove(lostList.indexOf((reserveList.get(i) - 1)));
+            } else if (lostList.contains(reserveList.get(i) + 1)) {
+                lostList.remove(lostList.indexOf((reserveList.get(i) + 1)));
+            }
+        }
+        answer = n - lostList.size();
+        return answer;
+    }
+
     public static void main(String[] args) {
-        추억_점수(new String[]{"may", "kein", "kain", "radi"}, new int[]{5, 10, 1, 3}, new String[][]{{"may"}, {"kein", "deny", "may"}, {"kon", "coni"}});
+        체육복(5, new int[]{2, 4}, new int[]{1, 3, 5});
     }
 }
