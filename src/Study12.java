c2a2892c886cd7e5a0f31cd955e97d66bc304ecb
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-03 19:04:43 +0900

    create : 덧칠하기

--- src/Study12.java
+++ src/Study12.java
@@ -18,6 +18,18 @@ public class Study12 {
         return answer;
     }
 
+    public int 덧칠하기(int n, int m, int[] section) {
+        int answer = 0;
+        int max = 0;
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] > max) {
+                answer++;
+                max = section[i] + m - 1;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         추억_점수(new String[]{"may", "kein", "kain", "radi"}, new int[]{5, 10, 1, 3}, new String[][]{{"may"}, {"kein", "deny", "may"}, {"kon", "coni"}});
     }
