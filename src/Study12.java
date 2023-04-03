54cab2d62b9d96561b32796949aca157dd4070f7
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-03 14:41:47 +0900

    create : 추억점수

--- src/Study12.java
+++ src/Study12.java
@@ -3,7 +3,18 @@ import java.util.HashMap;
 public class Study12 {
     public static int[] 추억_점수(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-
+        HashMap<String, Integer> hash = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            hash.put(name[i], yearning[i]);
+        }
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                if (hash.get(photo[i][j]) != null) {
+                    int a = hash.get(photo[i][j]);
+                    answer[i] += a;
+                }
+            }
+        }
         return answer;
     }
 
