421c6682334569580faa371a9a51234cc1c672f3
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-03 14:41:27 +0900

    create : Study12

---
+++ src/Study12.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Study12 {
+    public static int[] 추억_점수(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        추억_점수(new String[]{"may", "kein", "kain", "radi"}, new int[]{5, 10, 1, 3}, new String[][]{{"may"}, {"kein", "deny", "may"}, {"kon", "coni"}});
+    }
+}
