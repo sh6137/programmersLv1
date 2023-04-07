3ab2f706c4ef9a498384a45943ae7ba060dc9d6d
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-07 19:13:09 +0900

    create : 완주하지_못한_선수

--- src/Study13.java
+++ src/Study13.java
@@ -31,8 +31,27 @@ public class Study13 {
         return answer;
     }
 
+    public String 완주하지_못한_선수(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(completion);
+        LinkedList<String> participantSet = new LinkedList(Arrays.asList(participant));
+        participantSet.sort(Comparator.naturalOrder());
+        for (int i = 0; i < completion.length; i++) {
+            if (participantSet.get(0).equals(completion[i])) {
+                participantSet.remove(0);
+            } else if (participantSet.get(1).equals(completion[i])) {
+                participantSet.remove(1);
+            }
+        }
+        answer = participantSet.get(0);
+
+
+        return answer;
+    }
+
+
     public static void main(String[] args) {
         Study13 s = new Study13();
-        System.out.println(s.숫자_짝꿍("5525", "1255"));
+        s.완주하지_못한_선수(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"});
     }
 }
