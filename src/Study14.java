8e4baa5a8314445ede6b4811aac8009f9fe1c8f0
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-11 18:15:28 +0900

    create : 옹알이2

---
+++ src/Study14.java
@@ -0,0 +1,36 @@
+import java.util.Collection;
+import java.util.HashMap;
+
+public class Study14 {
+
+    public int 옹알이2(String[] babbling) {
+        int answer = babbling.length;
+        String[] exStr = {"aya", "ye", "woo", "ma"};
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < exStr.length; j++) {
+                babbling[i] = babbling[i].replace(exStr[j], String.valueOf(j));
+            }
+        }
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < babbling[i].length(); j++) {
+                if (Character.isAlphabetic(babbling[i].charAt(j))) {
+                    answer--;
+                    break;
+                } else if (j == babbling[i].length()-1) {
+                    
+                } else if (babbling[i].charAt(j) == babbling[i].charAt(j + 1)) {
+                    answer--;
+                    break;
+                }
+            }
+
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Study14 study14 = new Study14();
+        study14.옹알이2(new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"});
+    }
+}
