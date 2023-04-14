fedab0d403ef4371197aa5929ea9f845fda88d71
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-14 21:58:03 +0900

    create : 키패드 누르기 생성

--- src/Study14.java
+++ src/Study14.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class Study14 {
 
@@ -16,7 +19,7 @@ public class Study14 {
                 if (Character.isAlphabetic(babbling[i].charAt(j))) {
                     answer--;
                     break;
-                } else if (j == babbling[i].length()-1) {
+                } else if (j == babbling[i].length() - 1) {
 
                 } else if (babbling[i].charAt(j) == babbling[i].charAt(j + 1)) {
                     answer--;
@@ -30,6 +33,35 @@ public class Study14 {
 
     public String 카카오_인턴_키패드_누르기(int[] numbers, String hand) {
         String answer = "";
+        int right = 0;
+        int left = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 1 || numbers[i] == 4 || numbers[i] == 7) {
+                left = numbers[i];
+                answer += "L";
+            } else if (numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9) {
+                right = numbers[i];
+                answer += "R";
+            } else {
+                int[] center = new int[]{2, 5, 8, 0};
+                ArrayList<Integer> arrayList = new ArrayList<>(Arrays.stream(center).boxed().collect(Collectors.toList()));
+                if (arrayList.indexOf(left) != -1) {
+
+                } else if (arrayList.indexOf(right) != -1) {
+
+                }
+                int locationR = Math.abs(numbers[i] - right);
+                int locationL = Math.abs(numbers[i] - left);
+                if (locationL < locationR) {
+                    left = numbers[i];
+                    answer += "L";
+                } else if (locationR < locationL) {
+                    right = numbers[i];
+                    answer += "R";
+                }
+            }
+        }
 
         return answer;
     }
