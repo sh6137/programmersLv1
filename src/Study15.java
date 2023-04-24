6c0ea17d62d44a50338ade0adf73ffea6ad1259c
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-24 20:13:02 +0900

    create : 둘만의_암호

--- src/Study15.java
+++ src/Study15.java
@@ -1,3 +1,29 @@
 public class Study15 {
 
+    public String 둘만의_암호(String s, String skip, int index) {
+        String answer = "";
+        String[] str = s.split("");
+        String result = "";
+        for (String a : str) {
+            for (int i = 0; i < index; i++) {
+                int s1 = (int) a.charAt(0) + 1;
+                if (s1 == 123) {
+                    s1 = 97;
+                }
+                char ch = (char)s1;
+                if (skip.contains(String.valueOf(ch))) {
+                    i--;
+                }
+                a = String.valueOf(ch);
+            }
+            result += a;
+        }
+        answer = result;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Study15 study15 = new Study15();
+        study15.둘만의_암호("aukks","wbqd",5);
+    }
 }
