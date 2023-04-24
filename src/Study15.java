6e5c9e6cf390123ee56b8797b85550969721ce91
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-24 21:38:47 +0900

    feat : 대충_만든_자판

--- src/Study15.java
+++ src/Study15.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Study15 {
 
     public String 둘만의_암호(String s, String skip, int index) {
@@ -10,7 +13,7 @@ public class Study15 {
                 if (s1 == 123) {
                     s1 = 97;
                 }
-                char ch = (char)s1;
+                char ch = (char) s1;
                 if (skip.contains(String.valueOf(ch))) {
                     i--;
                 }
@@ -22,8 +25,23 @@ public class Study15 {
         return answer;
     }
 
+    public int[] 대충_만든_자판(String[] keymap, String[] targets) {
+        int[] answer = {};
+        for (String str : targets) {
+            for (int i = 0; i < str.length(); i++) {
+                String al = String.valueOf(str.charAt(i));
+                ArrayList<Integer> arrayList = new ArrayList<>();
+                for (String key : keymap) {
+                   arrayList.add(key.indexOf(al));
+                }
+                int a = Collections.min(arrayList);
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         Study15 study15 = new Study15();
-        study15.둘만의_암호("aukks","wbqd",5);
+        study15.둘만의_암호("aukks", "wbqd", 5);
     }
 }
