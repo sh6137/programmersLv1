f9b62f4758ff0d03f050edb429f2f6696bdbe2d0
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-28 15:43:29 +0900

    feat : 연금복권 번호 생성기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Random;
+
 public class Main {
+
+    public void pensionLotto(int quantity) {
+        Random random = new Random();
+        int[] jo = new int[quantity];
+        String[] number = new String[quantity];
+        //조 추첨
+        for (int i = 0; i < quantity; i++) {
+            jo[i] = random.nextInt(5) + 1;
+        }
+        //번호 추첨
+        for (int i = 0; i < quantity; i++) {
+            String str = "";
+            for (int j = 0; j < 6; j++) {
+                str += " " + String.valueOf(random.nextInt(10));
+            }
+            number[i] = str;
+        }
+
+        for (int i = 0; i < quantity; i++) {
+            System.out.println(jo[i] + "조"+ number[i]);
+        }
+    }
+
     public static void main(String[] args) {
+        Main main = new Main();
+        main.pensionLotto(5);
         System.out.println("Hello world!");
     }
 }
