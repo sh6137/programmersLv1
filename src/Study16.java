a478c0530311b82f9cfdc282c95dd03a27cb4e55
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-01 20:12:14 +0900

    feat : 대충만든자판

--- src/Study16.java
+++ src/Study16.java
@@ -3,21 +3,32 @@ import java.util.Collections;
 
 public class Study16 {
     public int[] 대충_만든_자판(String[] keymap, String[] targets) {
-        int[] answer = {};
+        int[] answer = new int[targets.length];
+        int count = 0;
         for (String str : targets) {
             for (int i = 0; i < str.length(); i++) {
                 String al = String.valueOf(str.charAt(i));
                 ArrayList<Integer> arrayList = new ArrayList<>();
-                for (String key : keymap) {
-                    arrayList.add(key.indexOf(al));
+                for (int j = 0; j < keymap.length; j++) {
+                    if (keymap[j].indexOf(al) != -1) {
+                        arrayList.add(keymap[j].indexOf(al));
+                    }
                 }
-                int a = Collections.min(arrayList);
+                if (arrayList.size() == 0) {
+                    answer[count] = -1;
+                    break;
+                } else {
+                    int a = Collections.min(arrayList) + 1;
+                    answer[count] = answer[count] + a;
+                }
             }
+            count++;
         }
         return answer;
     }
 
     public static void main(String[] args) {
-
+        Study16 study16 = new Study16();
+        study16.대충_만든_자판(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD", "AABB"});
     }
 }
