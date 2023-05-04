76abcc599a0aac951ccf9a9b70cb3f3a2dfb1cb4
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-04 14:27:25 +0900

    feat : 문자열 나누기 진행중

--- src/Study16.java
+++ src/Study16.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Study16 {
@@ -27,8 +28,35 @@ public class Study16 {
         return answer;
     }
 
+    public int 문자열_나누기(String s) {
+        int answer = 0;
+        int[] count = new int[26];
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            for (int j = 0; j < count.length; j++) {
+                int i1 = s.charAt(i) - 'a';
+                if (j != i1) {
+                    if (count[j] == count[i1]) {
+                        arrayList.add(s.substring(0, i + 1));
+                        s = s.substring(i + 1);
+                        i = -1;
+                        count = new int[26];
+                        break;
+                    }
+                }
+            }
+        }
+        if (s.length() != 0) {
+            arrayList.add(s);
+        }
+        answer = arrayList.size();
+        return answer;
+    }
+
     public static void main(String[] args) {
         Study16 study16 = new Study16();
-        study16.대충_만든_자판(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD", "AABB"});
+//        study16.대충_만든_자판(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD", "AABB"});
+        study16.문자열_나누기("aaabbaccccabba");
     }
 }
