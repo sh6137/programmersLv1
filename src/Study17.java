d71156cacb19e8e26a54803b52f8162aba92609b
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-08 20:32:06 +0900

    feat : 신규_아이디_추천

--- src/Study17.java
+++ src/Study17.java
@@ -26,6 +26,47 @@ public class Study17 {
         return answer;
     }
 
+    public String 신규_아이디_추천(String new_id) {
+        String answer = "";
+        //1단계
+        new_id = new_id.toLowerCase();
+        //2단계
+        String pattern = "[^a-z0-9-_.]";
+        new_id = new_id.replaceAll(pattern, "");
+        //3단계
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+        //4단계
+        if (new_id.charAt(0) == '.') {
+            new_id = new_id.substring(1);
+        }
+        if (new_id.length() > 0) {
+            if (new_id.charAt(new_id.length() - 1) == '.') {
+                new_id = new_id.substring(0, new_id.length() - 1);
+            }
+        }
+        //5단계
+        if (new_id.equals("")) {
+            new_id = "a";
+        }
+        //6단계
+        if (new_id.length() > 15) {
+            new_id = new_id.substring(0, 15);
+            if (new_id.charAt(new_id.length() - 1) == '.') {
+                new_id = new_id.substring(0, new_id.length() - 1);
+            }
+        }
+        //7단계
+        if (new_id.length() <= 2) {
+            while (new_id.length() < 3) {
+                new_id = new_id + new_id.charAt(new_id.length() - 1);
+            }
+        }
+        answer = new_id;
+        return answer;
+    }
+
     public static void main(String[] args) {
+        Study17 study17 = new Study17();
+        study17.신규_아이디_추천("abcdefghijklmn.p");
     }
 }
