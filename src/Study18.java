d63eb6bbf38eca3c708868c963d5b1bcbeb23e01
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-15 18:55:35 +0900

    feat : 달리기경주

---
+++ src/Study18.java
@@ -0,0 +1,9 @@
+public class Study18 {
+    public String[] 달리기_경주(String[] players, String[] callings) {
+        String[] answer = {};
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
