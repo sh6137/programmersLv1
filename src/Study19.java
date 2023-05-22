a40de1f722768ce683e4655b9910a231ac98484c
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-22 20:54:29 +0900

    create : 개인정보_수집_유효기간

---
+++ src/Study19.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class Study19 {
+
+    public int[] 개인정보_수집_유효기간(String today, String[] terms, String[] privacies) {
+        int[] answer = {};
+
+        HashMap<String, Integer> termsHash = new HashMap();
+        for (int i = 0; i < terms.length; i++) {
+            String [] a = terms[i].split(" ");
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
